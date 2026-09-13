ee31844727f777a6f2a8a1431c2ea865ea28b3e2
bazaarbachat5-png <bazaarbachat5@gmail.com>
2026-09-13 17:01:29 +0530

    Delete generateTryOnImage.js

--- generateTryOnImage.js
+++
@@ -1,3 +0,0 @@
-exports.handler = async function(event) {
-  return { statusCode: 410, headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}, body: JSON.stringify({error:'Try-On ab browser se free IDM-VTON Hugging Face Space ko use karta hai. Is Netlify function ki zarurat nahi hai.', errorCode:'TRYON_MOVED_TO_IDMVTON'}) };
-};
